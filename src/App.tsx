cf2ec8cd1097157d72c0cf9a28e8c55a3d40439d
helix-77 <mdatikmouhtasimrahi@gmail.com>
2025-03-21 17:28:18 +0600

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Upcoming from "./pages/Upcoming";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const App = () => (
   <BrowserRouter>
+    <Analytics />
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/upcoming" element={<Upcoming />} />
